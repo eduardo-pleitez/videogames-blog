54983c64598bffd24c10e55e22d2de24c6830edb
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2023-01-13 22:16:38 -0600

    :sparkles: Add search data function

--- javascript-files/main.js
+++ javascript-files/main.js
@@ -2,3 +2,13 @@ import { API_URL } from './globals.js';
 import getData from './getData.js';
 
 getData(`${API_URL}/videogames`, 'videogames-section');
+
+document.getElementById('search-game-button').addEventListener("click", searchData);
+
+function searchData() {
+  const gamesQuery = document.getElementById('search-game-input').value;
+  gamesQuery.trim();
+  if (gamesQuery !== '') {
+    getData(`${API_URL}/videogames?q=${gamesQuery}`, 'videogames-section');
+  }
+}
