6b18c96879ca38b62f336dcd50234a8c774f2ebc
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2023-01-19 12:11:17 -0600

    :art: Format main.js code

--- javascript-files/main.js
+++ javascript-files/main.js
@@ -2,34 +2,34 @@ import { API_URL } from './globals.js';
 import getData from './getData.js';
 import displayData from './displayData.js';
 
-let data = await getData(`${API_URL}/videogames`);
-
 const elementsPerPage = 10;
 const currentPage = 1;
 
+let data = await getData(`${API_URL}/videogames`);
+displayData(data, 'videogames-section', elementsPerPage, currentPage);
 addPagination();
 function addPagination() {
   const totalPages = Math.ceil(data.length / elementsPerPage);
   const pagesNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
   const paginationSection = document.getElementById('pagination-section');
-  if(pagesNumbers.length > 1){
-    paginationSection.innerHTML = pagesNumbers.map((pageNumber) =>
-    `<div class='pagination-button' onclick='changePage(${pageNumber})'>${pageNumber}</div>`
-    )
+  if (pagesNumbers.length > 1) {
+    paginationSection.innerHTML = pagesNumbers.map(
+      (pageNumber) =>
+        `<div class='pagination-button' onclick='changePage(${pageNumber})'>${pageNumber}</div>`
+    );
   } else {
     paginationSection.innerHTML = '';
   }
 }
-
-displayData(data, 'videogames-section', elementsPerPage, currentPage);
 
 function changePage(pageNumber) {
   displayData(data, 'videogames-section', elementsPerPage, pageNumber);
 }
 window.changePage = changePage;
 
-document.getElementById('search-game-button').addEventListener("click", searchData);
-
+document
+  .getElementById('search-game-button')
+  .addEventListener('click', searchData);
 async function searchData() {
   const gamesQuery = document.getElementById('search-game-input').value;
   gamesQuery.trim();
